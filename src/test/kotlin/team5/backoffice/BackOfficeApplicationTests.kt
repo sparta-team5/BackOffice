20c8bf930cde1416b2f870ce2c6776784b4f5bb5
HR <dev.muun07@gmail.com>
2024-06-11 20:54:31 +0900

    🎉create: Init Project

---
+++ src/test/kotlin/team5/backoffice/BackOfficeApplicationTests.kt
@@ -0,0 +1,13 @@
+package team5.backoffice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackOfficeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
