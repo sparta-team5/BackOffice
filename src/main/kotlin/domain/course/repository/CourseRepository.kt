f8abb10a91eb19f4f861ed86684a9547a08d4465
pcouqc <nagapen@nate.com>
2024-06-12 19:58:52 +0900

    :pushpin: chore : Add annotation for Repository

--- src/main/kotlin/domain/course/repository/CourseRepository.kt
+++ src/main/kotlin/domain/course/repository/CourseRepository.kt
@@ -2,6 +2,8 @@ package domain.course.repository
 
 import domain.course.model.Course
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface CourseRepository : JpaRepository<Course, Long> {
 }
