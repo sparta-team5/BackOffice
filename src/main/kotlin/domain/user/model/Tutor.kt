61360549072ed459023dca001ee4f2dfe77a98f9
HR <dev.muun07@gmail.com>
2024-06-13 02:16:47 +0900

    🐛fix: Fix Errors and Change Variables Type

--- src/main/kotlin/domain/user/model/Tutor.kt
+++ src/main/kotlin/domain/user/model/Tutor.kt
@@ -4,30 +4,30 @@ import jakarta.persistence.*
 
 
 @Entity
-@Table(name ="tutor") //todo
+@Table(name = "tutor") //todo
 class Tutor(
 
-    @Column(name="nickname", nullable=false)
-    var nickname:String,
+    @Column(name = "nickname", nullable = false)
+    var nickname: String,
 
-    @Column(name="email", nullable=false)
-    val email:String,
+    @Column(name = "email", nullable = false)
+    val email: String,
 
-    @Column(name="password", nullable=false)
-    var password:String,
+    @Column(name = "password", nullable = false)
+    var password: String,
 
-    @Column(name="description", nullable=false)
-    var description:String,
+    @Column(name = "description", nullable = false)
+    var description: String,
 
-    @Column(name="career", nullable=false)
-    var career:String,
+    @Column(name = "career", nullable = false)
+    var career: String,
 
-    @Column(name="prev_passwords", nullable=true)
-    val prev_passwords:String,
+    @Column(name = "prev_passwords", nullable = true)
+    val prevPasswords: String,
 ) {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id:Long? =null
+    var id: Long? = null
 
 }
