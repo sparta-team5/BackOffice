ad50a49478b9ffd98d5da5f11b515872f46e73f2
pcouqc <nagapen@nate.com>
2024-06-12 12:52:55 +0900

    :tada: create : Add DTOs for paging and filtering

---
+++ src/main/kotlin/domain/course/dto/CursorDto.kt
@@ -0,0 +1,7 @@
+package domain.course.dto
+
+data class CursorDto(
+    val cursor: Int = 0,
+    val page: Int = 10,
+    val orderBy: String = "createdAt", // createdAt, viewCount, rate
+)
---
+++ src/main/kotlin/domain/course/dto/FilteringDto.kt
@@ -0,0 +1,8 @@
+package domain.course.dto
+
+data class FilteringDto(
+    val title : String,
+    val tutorName : String,
+    val category : Category,
+    val rate : Int
+)
