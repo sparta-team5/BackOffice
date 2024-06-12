8789251de9d830737918167349cf25c6db0cdb6e
pcouqc <nagapen@nate.com>
2024-06-12 19:58:23 +0900

    :bug: fix : Add }(scope) for it was missing

--- src/main/kotlin/domain/course/service/CourseService.kt
+++ src/main/kotlin/domain/course/service/CourseService.kt
@@ -16,9 +16,9 @@ import java.time.LocalDateTime
 
 @Service
 class CourseService(
-    val courseRepository: CourseRepository,
-    val studentRepository: StudentRepository,
-    val tutorRepository: TutorRepository,
+    private val courseRepository: CourseRepository,
+    private val studentRepository: StudentRepository,
+    private val tutorRepository: TutorRepository,
 ) {
 
     fun getAllCourses(cursor: CursorRequest, studentId: Long?): CursorPageResponse {
@@ -62,102 +62,105 @@ class CourseService(
                     existBookmark(studentId, it.id!!),
                     existSubscribe(studentId, it.id)
                 )
-            } else {
-                courseSlice.content.map { it.toListResponse(false, false) }
+            }
+        } else {
+            courseSlice.content.map {
+                it.toListResponse(false, false)
             }
         }
-        return CursorPageResponse(pageResponse, nextCursor)
     }
+    return CursorPageResponse(pageResponse, nextCursor)
+}
 
-    fun createCourse(request: CourseRequest, tutorId: Long): CourseSimpleResponse {
-        val tutor = tutorRepository.findByIdOrNull(tutorId)
-            ?: throw RuntimeException("Tutor not found")
+fun createCourse(request: CourseRequest, tutorId: Long): CourseSimpleResponse {
+    val tutor = tutorRepository.findByIdOrNull(tutorId)
+        ?: throw RuntimeException("Tutor not found")
 
-        val course = courseRepository.save(
-            Course(
-                title = request.title,
-                tutor = tutor,
-                description = request.description,
-                category = request.category,
-                imageUrl = request.imageUrl,
-                viewCount = 0,
-                rate = null,
-                createdAt = LocalDateTime.now()
-            )
-        ).toSimpleResponse()
+    val course = courseRepository.save(
+        Course(
+            title = request.title,
+            tutor = tutor,
+            description = request.description,
+            category = request.category,
+            imageUrl = request.imageUrl,
+            viewCount = 0,
+            rate = null,
+            createdAt = LocalDateTime.now()
+        )
+    ).toSimpleResponse()
+}
+
+@Transactional
+fun updateCourseById(courseId: Long, request: CourseRequest, tutorId: Long): CourseSimpleResponse {
+    val course = courseRepository.findByIdOrNull(courseId)
+        ?: throw RuntimeException("Course not found")
+    if (tutorId != course.tutor.id) {
+        throw RuntimeException("No Permission")
     }
-
-    @Transactional
-    fun updateCourseById(courseId: Long, request: CourseRequest, tutorId: Long): CourseSimpleResponse {
-        val course = courseRepository.findByIdOrNull(courseId)
-            ?: throw RuntimeException("Course not found")
-        if (tutorId != course.tutor.id) {
-            throw RuntimeException("No Permission")
-        }
-        course.apply {
-            title = request.title
-            description = request.description
-            category = request.category
-            imageUrl = request.imageUrl
-        }
-        println("Course ${course.id} has been successfully updated")
-        return course.toSimpleResponse()
+    course.apply {
+        title = request.title
+        description = request.description
+        category = request.category
+        imageUrl = request.imageUrl
     }
+    println("Course ${course.id} has been successfully updated")
+    return course.toSimpleResponse()
+}
 
-    fun deleteCourseById(courseId: Long, tutorId: Long) {
-        val course = courseRepository.findByIdOrNull(courseId)
-            ?: throw RuntimeException("Course not found")
-        if (tutorId != course.tutor.id) {
-            throw RuntimeException("No Permission")
-        }
-        courseRepository.delete(course)
-        println("Course ${course.id} has been successfully deleted")
+fun deleteCourseById(courseId: Long, tutorId: Long) {
+    val course = courseRepository.findByIdOrNull(courseId)
+        ?: throw RuntimeException("Course not found")
+    if (tutorId != course.tutor.id) {
+        throw RuntimeException("No Permission")
     }
+    courseRepository.delete(course)
+    println("Course ${course.id} has been successfully deleted")
+}
 
-    // ERD 찾아보니 따로 bookmark 에 id가 존재하지 않아서 이런 로직을 짜봤습니다
-    fun addBookmark(courseId: Long, studentId: Long) {
-        val course = courseRepository.findByIdOrNull(courseId)
-            ?: throw RuntimeException("Course not found")
-        val student = studentRepository.findByIdOrNull(studentId)
-            ?: throw RuntimeException("Student not found")
-        student.apply {
-            student.bookmark.add(course)
-        }
-        studentRepository.save(student)
-        println("Course ${course.id} is now marked")
+// ERD 찾아보니 따로 bookmark 에 id가 존재하지 않아서 이런 로직을 짜봤습니다
+fun addBookmark(courseId: Long, studentId: Long) {
+    val course = courseRepository.findByIdOrNull(courseId)
+        ?: throw RuntimeException("Course not found")
+    val student = studentRepository.findByIdOrNull(studentId)
+        ?: throw RuntimeException("Student not found")
+    student.apply {
+        student.bookmark.add(course)
     }
+    studentRepository.save(student)
+    println("Course ${course.id} is now marked")
+}
 
-    fun removeBookmark(courseId: Long, studentId: Long) {
-        val course = courseRepository.findByIdOrNull(courseId)
-            ?: throw RuntimeException("Course not found")
-        val student = studentRepository.findByIdOrNull(studentId)
-            ?: throw RuntimeException("Student not found")
-        student.apply {
-            student.bookmark.remove(course)
-        }
-        studentRepository.save(student)
-        println("Course ${course.id} is now unmarked")
+fun removeBookmark(courseId: Long, studentId: Long) {
+    val course = courseRepository.findByIdOrNull(courseId)
+        ?: throw RuntimeException("Course not found")
+    val student = studentRepository.findByIdOrNull(studentId)
+        ?: throw RuntimeException("Student not found")
+    student.apply {
+        student.bookmark.remove(course)
     }
+    studentRepository.save(student)
+    println("Course ${course.id} is now unmarked")
+}
 
-    fun subscribe(courseId: Long, studentId: Long) {
-        val course = courseRepository.findByIdOrNull(courseId)
-            ?: throw RuntimeException("Course not found")
-        val student = studentRepository.findByIdOrNull(studentId)
-            ?: throw RuntimeException("Student not found")
-        if (!existSubscribe(studentId, courseId)) {
-            throw RuntimeException("Already subscribed")
-        }
-        student.apply {
-            student.subscribe.add(course)
-        }
-        println("Course ${course.id} is now subscribed")
+fun subscribe(courseId: Long, studentId: Long) {
+    val course = courseRepository.findByIdOrNull(courseId)
+        ?: throw RuntimeException("Course not found")
+    val student = studentRepository.findByIdOrNull(studentId)
+        ?: throw RuntimeException("Student not found")
+    if (!existSubscribe(studentId, courseId)) {
+        throw RuntimeException("Already subscribed")
     }
+    student.apply {
+        student.subscribe.add(course)
+    }
+    println("Course ${course.id} is now subscribed")
+}
 
-    private fun existBookmark(studentId: Long, courseId: Long): Boolean {
-        // TODO : bookmark 에 student 와 course 를 가진 개체가 있는지 확인
-    }
+private fun existBookmark(studentId: Long, courseId: Long): Boolean {
+    // TODO : bookmark 에 student 와 course 를 가진 개체가 있는지 확인
+}
 
-    private fun existSubscribe(studentId: Long, courseId: Long): Boolean {
-        // TODO : subscription 에 student 와 course 를 가진 개체가 있는지 확인
-    }
+private fun existSubscribe(studentId: Long, courseId: Long): Boolean {
+    // TODO : subscription 에 student 와 course 를 가진 개체가 있는지 확인
+}
 }
