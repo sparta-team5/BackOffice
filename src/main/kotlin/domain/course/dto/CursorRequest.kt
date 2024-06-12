1bf6a2a4d932fbdad49c9bbfdde708fa64e48e10
pcouqc <nagapen@nate.com>
2024-06-12 21:29:22 +0900

    :tada: create : Add enum class for paging sort

--- src/main/kotlin/domain/course/dto/CursorRequest.kt
+++ src/main/kotlin/domain/course/dto/CursorRequest.kt
@@ -1,7 +1,10 @@
 package domain.course.dto
 
+import domain.course.model.SortType
+import javax.swing.SortOrder
+
 data class CursorRequest(
     val cursor: Int = 0,
     val page: Int = 10,
-    val orderBy: String = "createdAt", // createdAt, viewCount, rate + desc
+    val orderBy: SortType
 )
