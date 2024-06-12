ab18d264ffc2ab38f1d138244ff495fd705066fa
HR <dev.muun07@gmail.com>
2024-06-13 02:21:21 +0900

    ✨feat: Add Tutor Controller & Service

---
+++ src/main/kotlin/domain/auth/tutor/controller/TutorController.kt
@@ -0,0 +1,44 @@
+package domain.auth.tutor.controller
+
+import domain.auth.dto.ChangePasswordRequest
+import domain.auth.dto.LoginRequest
+import domain.auth.dto.SignUpRequest
+import domain.auth.tutor.service.TutorService
+import domain.user.dto.TutorResponseDto
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/auth/tutor")
+class TutorController(
+    private val tutorService: TutorService
+) {
+    @GetMapping("/signup")
+    fun signUpTutor(
+        @RequestBody signUpRequest: SignUpRequest
+    ): ResponseEntity<TutorResponseDto> {
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(tutorService.signUpTutor(signUpRequest))
+    }
+
+    @GetMapping("/login")
+    fun loginTutor(
+        @RequestBody loginRequest: LoginRequest
+    ): ResponseEntity<String> {
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(tutorService.loginTutor(loginRequest))
+    }
+
+    @PatchMapping("/change-password")
+    fun changePassword(
+        @RequestBody changePasswordRequest: ChangePasswordRequest
+    ): ResponseEntity<Boolean> {
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(tutorService.changeTutorPassword(changePasswordRequest))
+
+    }
+}
