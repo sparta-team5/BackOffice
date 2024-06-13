3dfc0afd24814e2fb51d20a9630df71b9b15955a
HR <dev.muun07@gmail.com>
2024-06-13 09:39:24 +0900

    🚚Rename: Rename Controllers

--- src/main/kotlin/domain/auth/tutor/controller/TutorAuthController.kt
+++ src/main/kotlin/domain/auth/tutor/controller/TutorAuthController.kt
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/auth/tutor")
-class TutorController(
+class TutorAuthController(
     private val tutorService: TutorService
 ) {
     @PostMapping("/signup")
