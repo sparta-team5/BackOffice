c9316b567f802c7249d46ab9ccb1a052dc399205
AWKRID <minzoo202738@gmail.com>
2024-06-13 14:30:47 +0900

    :bug: fix: Update UserService code format

--- src/main/kotlin/domain/user/service/UserService.kt
+++ src/main/kotlin/domain/user/service/UserService.kt
@@ -12,39 +12,28 @@ class UserService(
     private val studentRepository: StudentRepository,
     private val tutorRepository: TutorRepository,
 ) {
-
-
     fun getStudentById(studentId: Long): StudentResponse {
         val student = studentRepository.findByIdOrNull(studentId) ?: throw RuntimeException("Student")
-
         return StudentResponse.from(student)
     }
 
 
     @Transactional
     fun updateStudentById(studentId: Long, request: UpdateStudentRequest): StudentResponse {
-
         val student = studentRepository.findByIdOrNull(studentId) ?: throw RuntimeException("Student")
         //todo : null이면 예외 던지기
-
         //todo : user가져오기
         //todo : 본인이 아니면 throw IllegalAccessException
-
-
         val nickname = request.nickname
         return StudentResponse.from(student)
-
     }
 
     fun deleteStudentById(studentId: Long) {
         val student = studentRepository.findByIdOrNull(studentId) ?: throw RuntimeException("Student")
         //todo : null이면 예외 던지기
-
         //todo : 토큰에서 user가져오기
         //todo : 본인이 아니면 throw IllegalAccessException
-
         studentRepository.delete(student)
-
     }
 
     //todo : admin auth ?
@@ -53,48 +42,36 @@ class UserService(
 //    }
 
     fun getTutorById(tutorId: Long): TutorResponse {
-
         val tutor = tutorRepository.findByIdOrNull(tutorId) ?: throw RuntimeException("Tutor")
-
         return TutorResponse.from(tutor)
-
     }
 
     fun updateTutorById(tutorId: Long, request: UpdateTutorRequest): TutorResponse {
-
         val tutor = tutorRepository.findByIdOrNull(tutorId) ?: throw RuntimeException("Tutor")
-
         //todo : null이면 예외 던지기
-
         //todo : 토큰에서 user가져오기
         //todo : 본인이 아니면 throw IllegalAccessException
-
         tutor.apply {
             nickname = request.nickname
             description = request.description
             career = request.career
 
         }
-
         return TutorResponse.from(tutor)
-
     }
 
     fun deleteTutorById(tutorId: Long) {
-
         val tutor = tutorRepository.findByIdOrNull(tutorId) ?: throw RuntimeException("Tutor")
         //todo : null이면 예외 던지기
 
         //todo : 토큰에서 user가져오기
         //todo : 본인이 아니면 throw IllegalAccessException
-
         tutorRepository.delete(tutor)
     }
 
     //todo
     fun followStudentAndUser(tutorId: Long): FollowResponse {
         val tutor = tutorRepository.findByIdOrNull(tutorId) ?: throw RuntimeException("Tutor")
-
         //todo : follow 요청한 Userid 토큰에서 가져오기
         TODO()
 
@@ -102,7 +79,6 @@ class UserService(
 
     fun unfollowStudentAndUser(tutorId: Long) {
         val tutor = tutorRepository.findByIdOrNull(tutorId) ?: throw RuntimeException("Tutor")
-
         //todo : unfollow 요청한 Userid 토큰에서 가져오기
 
     }
