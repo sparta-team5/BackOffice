b488f510dd0faee2b63195d88645ce3a9e2ac2cd
AWKRID <minzoo202738@gmail.com>
2024-06-17 11:38:20 +0900

    :sparkles: feat: Update User Response DTO

--- src/main/kotlin/team5/backoffice/domain/user/dto/TutorResponse.kt
+++ src/main/kotlin/team5/backoffice/domain/user/dto/TutorResponse.kt
@@ -4,13 +4,19 @@ import team5.backoffice.domain.user.model.Tutor
 
 data class TutorResponse(
     val id: Long,
-    val email: String
+    val email: String,
+    val nickname: String,
+    val career: String,
+    val description: String,
 ) {
 
     companion object {
         fun from(tutor: Tutor): TutorResponse = TutorResponse(
             id = tutor.id!!,
-            email = tutor.email
+            email = tutor.email,
+            nickname = tutor.nickname,
+            career = tutor.career,
+            description = tutor.description
         )
     }
 }
